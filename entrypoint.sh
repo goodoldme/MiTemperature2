f1a2d1256d75089bcd4a555209e7b85820f01652
Calvin Hobbes <goodoldme@homelab>
2025-12-10 11:23:16 +0000

    Docker changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 /app/LYWSD03MMC.py -a --devicelistfile /app/sensors.ini --influxdb 1 -r -b --callback sendToInflux.sh
+python3 /app/MiTemperature2.py --devicelistfile /app/sensors.ini --influxdb 1 -r -b --callback sendToInflux.sh
